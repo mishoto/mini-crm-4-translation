30995625dff3b1a74b3e828afaa5fb2d4d74197b
mishoto <mihail.dev.io@gmail.com>
2022-09-06 07:39:14 +0300

    add AuthProvider to Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import SignUp from '../components/sign-up/Signup'
+import { AuthProvider } from '../context/authContext'
 
 const Home = () => {
   return (
-    <>
+    <AuthProvider>
     <SignUp/>
-    </>
+    </AuthProvider>
   )
 }
 
